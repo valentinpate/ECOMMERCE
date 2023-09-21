13ec0d7ba354ab9f58d7eddf6cd2ac8368859361
unknown <barchettaleonel@gmailcom>
2023-09-21 13:36:18 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// Importo los paquetes
+const express=require("express")
+const mongoose=require("mongoose")
+
+// Importo el modulo de rutas
+const authRoutes=require("./routes/authRoutes")
+
+// Guardo el express en una constante
+const app=express()
+
+// Hago que los archivos de la carpeta public sean estaticos
+app.use(express.static("public"))
+
+// Seteo el motor de plantilla
+app.set("view engine","ejs")
+
+// Conexion de la base de datos mongo DB
+// const dbURL=""
+// mongoose.connect(dbURL)
+// .then((result)=>app.listen(4000))
+// .catch((error)=>console.log(error))
+
+// Agrego la primera ruta
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+// Conecto las rutas
+app.use(authRoutes)
+
+// levanto el servidor
+app.listen(4000,()=>{
+    console.log("servidor ejecutandose")
+})
+
+
+
---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+// Importo el express y lo guardo en la constante router
+const {Router}=require("express")
+const router=Router()
+
+// Seteo las rutas/direcciones
+router.post("/signup",(req,res)=>{})
+router.get("/signup",(req,res)=>{})
+
+router.post("/signin",(req,res)=>{})
+router.get("/signin",(req,res)=>{})
+
+// Exporto las rutas
+module.exports=router
