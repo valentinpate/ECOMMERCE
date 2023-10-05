49d9037595f8369598c919ed4a8afb9f5c82c235
unknown <barchettaleonel@gmailcom>
2023-10-05 12:01:27 -0300

    commit leo

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,17 +3,29 @@ const User=require("../models/User")
 
 // Manejo de errores
 const handleErrors=(err)=>{
+    // se imprime el mensaje de error y el código del error en la consola.
     console.log(err.message,err.code)
-    let error = {email:"",name:"",password:"",user:"",phone:""}
+
+    // se inicializa un objeto llamado "error" con propiedades para diferentes campos. Esto se utilizará para almacenar mensajes de error específicos para cada campo.
+    let error = {email:"",name:"",user:"",password:"",phone:"",region:""}
 
     // Duplicaion de email
-    if(err.code===11000){
-        error.email="Este email ya esta en uso"
-        return error
+    if (err.code === 11000) {
+        // Si el error tiene el código 11000, se asume que es un error de duplicación de un valor único en la base de datos, como un email duplicado. En este caso, se establece el mensaje de error correspondiente en la propiedad "email" del objeto "error".
+        if (err.message.includes("email")) {
+            error.email = "Este email ya está en uso";
+        }
+        if (err.message.includes("user")) {
+            error.user = "Este usuario ya está en uso";
+        }
+        
+
+        return error;
     }
 
     // Validacion de errores
     if(err.message.includes("Usuarios validation failed")){
+        // Si el error es una validación de errores generados por el modelo de usuario ("Usuarios validation failed"), se procesan estos errores y se almacenan los mensajes de error específicos para cada campo en el objeto "error".
         object.values(err.errors).forEach(({properties})=>{
             error[properties.path]=properties.message
         })
@@ -24,11 +36,11 @@ const handleErrors=(err)=>{
 // Se exporta la logica de las rutas a authRoutes
 module.exports.signup_post= async(req,res)=>{
     // Destructuring
-    const {email,name,user,password,phone}=req.body
+    const {email,name,user,password,phone,region}=req.body
 
     // Se usa un TRY y un CATCH para el manejo de errores
     try{
-        const users= await User.create({email,name,user,password,phone})
+        const users= await User.create({email,name,user,password,phone,region})
         res.status(201).json(users)
     }
     catch(err){
@@ -39,7 +51,7 @@ module.exports.signup_post= async(req,res)=>{
 }
 
 module.exports.signup_get=(req,res)=>{
-    
+    res.render("signup")
 }
 
 
