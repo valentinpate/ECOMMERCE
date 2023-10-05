0033b65d65bc42d47823b88b98b06661b1effc65
enzoburgos <enzoburgos960@gmail.com>
2023-10-05 19:16:53 -0300

    agregue html y css de page contacto y carrito con comentarios de su ruta

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,13 +6,13 @@ const router=Router()
 const authControllers=require("../controllers/authControllers")
 
 // Seteo las rutas/direcciones
-router.post("/signup",authControllers.signup_post)
-router.get("/signup",authControllers.signup_get)
+// router.post("/signup",authControllers.signup_post)
+// router.get("/signup",authControllers.signup_get)
 
-// router.post("/",authControllers.login_post)
+// router.post("/signin",authControllers.login_post)
+// router.get("/signin",authControllers.login_get)
 
-router.post("/signin",authControllers.login_post)
-router.get("/signin",authControllers.login_get)
+router.post("/",authControllers.login_post)
 
 //ruta para la page ofertas
 router.get("/ofertas",authControllers.ofertas_get)
@@ -23,16 +23,15 @@ router.get("/product",authControllers.product_get)
 //ruta para la page home 
 router.get("/home",authControllers.home_get)
 
+//ruta para la page mi perfil (falta agregar la funcion a authcontrollers)
 router.get("/miperfil",(req,res)=>{
   res.render("miperfil")
 })
-//ruta para la page mi perfil (falta agregar la funcion a authcontrollers)
+
 router.get("/informacion",(req,res)=>{
   res.render("informacion")
 })
 
-// Exporto las rutas
-module.exports=router
 //rutas contacto y carrito
 // router.get("/contacto",(req,res)=>{
 //   res.render("contacto")    
@@ -40,3 +39,7 @@ module.exports=router
 // router.get("/carrito",(req,res)=>{
 //   res.render("carrito")
 // })
+
+// Exporto las rutas
+module.exports=router
+
